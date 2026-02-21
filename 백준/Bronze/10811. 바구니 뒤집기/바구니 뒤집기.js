13c978245ba2b7fcd9dedd713199fd7703f3bedd
올챙이 <126138033+sojeong0302@users.noreply.github.com>
2026-02-21 22:03:55 +0900

    [Bronze II] Title: 바구니 뒤집기, Time: 108 ms, Memory: 9384 KB -BaekjoonHub

--- 백준/Bronze/10811. 바구니 뒤집기/바구니 뒤집기.js
+++ 백준/Bronze/10811. 바구니 뒤집기/바구니 뒤집기.js
@@ -1,23 +1,24 @@
 const fs = require("fs");
-const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
-let input = fs.readFileSync(filePath).toString().trim().split("\n");
+const input = fs.readFileSync(0, "utf8").trim().split("\n");
 
+// N과 M 입력
 const [N, M] = input[0].split(" ").map(Number);
 
-let basket = Array(N)
-  .fill(1)
-  .map((a, b) => a + b);
-
-for (let i = 1; i <= M; i++) {
-  let [a, b] = input[i].split(" ").map(Number);
-  let arr = [];
-
-  for (let j = a - 1; j < b; j++) {
-    arr.push(basket[j]);
-  }
-
-  arr.reverse();
-  basket.splice(a - 1, b - a + 1, ...arr);
+// 바구니 배열 만들기
+let arr = [];
+for (let i = 0; i < N; i++) {
+  arr[i] = i + 1;
 }
 
-console.log(basket.join(" "));
+//i와 j 입력
+for (let k = 0; k < M; k++) {
+  const [i, j] = input[k + 1].split(" ").map(Number);
+  //역순
+  for (let x = 0; x < (j - i + 1) / 2; x++) {
+    let temp = arr[i - 1 + x]; //i번째 값 백업
+    arr[i - 1 + x] = arr[j - 1 - x]; //i번째값에 j번째에 있는 값 할당
+    arr[j - 1 - x] = temp; //j번째에 i번째 값 할당
+  }
+}
+
+console.log(arr.join(" "));
